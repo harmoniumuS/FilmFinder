97898beac3a25855d32c5ea70cbed1bfba305a77
harmoniumuS <gimme.wards@yandex.ru>
2024-11-02 12:47:21 +0300

    Добавьте файлы проекта.

---
+++ FilmFinder/Models/Movie.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilmFinder.Models
+{
+    public class Movie
+    {
+        public int KinopoiskId { get; set; }
+        
+        public string KinopoiskHDId { get; set; }
+
+        public string ImdbId { get; set; }
+
+        public string NameRu { get; set; }
+
+        public string NameEn { get; set; }
+
+        public string NameOriginal { get; set; }
+        public string PosterUrl { get; set; }
+        public string PosterUrlPreview { get; set; }
+        public string CoverUrl { get; set; }
+        public string LogoUrl { get; set; }
+        public int ReviewsCount { get; set;}
+        public double RatingGoodReview { get; set;}
+        public int RatingGoodReviewVoteCount { get; set; }
+        public double RatingKinopoisk { get; set; }
+        public int RatingKinopoiskVoteCount { get; set; }
+        public double RatingImdb { get; set;}
+        public int RatingImdbVoteCount { get; set; }
+        public double RatingFilmCritics { get; set; }
+        public int RatingFilmCriticsVoteCount { get; set; }
+        public double RatingAwait { get; set; }
+        public int RatingAwaitCount { get; set;}
+
+        public double RatingRfCritics { get; set; }
+        public int RatingRfCriticsVoteCount { get; set; }
+        public string WebUrl { get; set;}
+        public int Year { get; set; }
+        public int FilmLength { get; set; }
+        public string Slogan { get; set; }
+
+        public string Description { get; set;}
+        public string ShortDescription { get; set; }
+        public string EditorAnnotation { get; set; }
+        public bool IsTicketsAvailable { get; set; }
+        public ProductionStatus ProductionStatus { get; set; }
+        public Type Type { get; set; }
+        public string RatingMpaa { get; set; }
+        public string RatingAgeLimits { get; set; }
+        public bool HasImax { get; set;}
+        public bool Has3D { get; set;}
+        public string LastSync { get; set;}
+        public List<Country> Countries { get; set; } 
+        public List<Genre> Genres { get; set; }
+
+        public int StartYear { get; set; }
+        public int EndYear { get; set;}
+    }
+}
---
+++ FilmFinder/ViewModel/MainViewModel.cs
@@ -0,0 +1,24 @@
+﻿using FilmFinder.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilmFinder.ViewModel
+{
+    public class MainViewModel
+    {
+        public ObservableCollection<Movie> Movies { get; set;}
+        public ObservableCollection<Movie> Favorites { get; set; }
+
+        public MainViewModel()
+        {
+            Movies = new ObservableCollection<Movie>();
+            Favorites = new O
+
+        }
+
+    }
+}
